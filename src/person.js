5e70f04e7212d00cb16f7584cedc409f506274b0
kareem-ahmxd <kareemraslan500@gmail.com>
2025-03-15 12:26:13 +0200

    Create person.js

---
+++ src/person.js
@@ -0,0 +1,13 @@
+class Person {
+  constructor(name, age, location) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
+
+  displayInfo() {
+    console.log(Name: ${this.name});
+    console.log(Age: ${this.age});
+    console.log(Location: ${this.location});
+  }
+}
